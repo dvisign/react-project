b7c937a76673941fbbaedbeaa20883e692e560ad
gline <jmch0922@gmail.com>
2019-10-23 16:53:11 +0900

    react costomer props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Costomer from './components/Costomer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = {
+  "name":"gline",
+  "birthday" : "880922",
+  "gender" : "남",
+  "job" : "직장인"
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Costomer 
+        name = {customer.name}
+        birthday = {customer.birthday}
+        gender = {customer.gender}
+        job = {customer.job}
+      />
+    )
+  }
 }
 
 export default App;
